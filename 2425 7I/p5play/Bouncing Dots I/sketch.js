138ee5dbb90b5446094507092439e516cf787873
Andy Lightcap <andy.lightcap@gmail.com>
2024-12-04 14:23:15 -0500

    finished I block project

--- 2425 7I/p5play/Bouncing Dots I/sketch.js
+++ 2425 7I/p5play/Bouncing Dots I/sketch.js
@@ -1,27 +1,37 @@
-// let ball;
-let dot;
+let dots = [];
+let numDots = 15;
+let colors = ['#ccdbdc', '#80ced7', '#63c7b2', '#8e6c88'];
 
 function setup() {
 	new Canvas(800, 600);
-	// new Canvas(500, 500);
-	// displayMode('centered');
 
-	// ball = new Sprite();
-	// ball.diameter = 50;
-	dot = new Sprite();
-	dot.color = 'red';
-	dot.radius = 25;
-	dot.x = width / 2;
-	dot.y = height / 2;
-	// dot.direction = 90;
-	dot.direction = random(360);
-	// dot.speed = 5;
-	dot.speed = random(5, 10);
+	for (let i = 0; i < numDots; i++) {
+		dots.push(createDot());
+	}
+	// dot = createDot();
 }
 
 function draw() {
-	background('skyblue');
+	background('#263d42');
 
+	for (let dot of dots) {
+		updateDot(dot);
+	}
+}
+
+function createDot() {
+	let	dot = new Sprite();
+	dot.color = random(colors);
+	dot.radius = 25;
+	dot.x = width / 2;
+	dot.y = height / 2;
+	dot.direction = random(360);
+	dot.speed = random(5, 10);
+	dot.overlaps(allSprites);
+	return dot;
+}
+
+function updateDot(dot) {
 	if (dot.x < 0 + dot.radius) {
 		dot.velocity.x *= -1;
 	}
@@ -37,11 +47,4 @@ function draw() {
 	if (dot.y > height - dot.radius) {
 		dot.velocity.y *= -1;
 	}
-
-	// background('skyblue');
-
-	// if (mouse.presses()) {
-	// 	ball.speed = 10;
-	// 	ball.moveTo(mouse);
-	// }
 }
