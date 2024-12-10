2a839be3bc04d2541b019b48c106cfa74a9223df
Andy Lightcap <andy.lightcap@gmail.com>
2024-12-10 08:49:44 -0500

    updated peg arrangement and added visual when peg is struck

--- Personal Projects/Pinball/sketch.js
+++ Personal Projects/Pinball/sketch.js
@@ -1,18 +1,20 @@
 let ball;
 let pegs;
 let pegLayout = [
-	[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0],
-	[0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-	[0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+	[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+	[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
 ];
 
 let pegSpacing;
-let leftBound;
-let rightBound;
+// let leftBound;
+// let rightBound;
 
 
 function setup() {
@@ -22,8 +24,8 @@ function setup() {
 
 	pegSpacing = width / (pegLayout[0].length + 1);
 
-	ball = createBall();
 	pegs = setupPegs();
+	ball = createBall();
 	
 	for (let i = 0; i < pegLayout.length; i++) {
 		for (let j = 0; j < pegLayout[0].length; j++) {
@@ -35,29 +37,30 @@ function setup() {
 		}
 	}
 
-	leftBound = new Sprite();
-	leftBound.x = pegs[10].x - pegSpacing;
-	leftBound.y = pegs[10].y - 2 * pegSpacing;
-	leftBound.collider = 'static';
-	leftBound.rotation = 45;
-	leftBound.height = 400;
-	leftBound.color = 'gold';
-	leftBound.bounciness = 1;
+	// leftBound = new Sprite();
+	// leftBound.x = pegs[10].x - pegSpacing;
+	// leftBound.y = pegs[10].y - 2 * pegSpacing;
+	// leftBound.collider = 'static';
+	// leftBound.rotation = 45;
+	// leftBound.height = 400;
+	// leftBound.color = 'gold';
+	// leftBound.bounciness = 1;
 
-	rightBound = new Sprite();
-	rightBound.x = pegs[14].x + pegSpacing;
-	rightBound.y = pegs[14].y - 2 * pegSpacing; 
-	rightBound.collider = 'static';
-	rightBound.rotation = -45;
-	rightBound.height = 400;
-	rightBound.color = 'gold';
-	rightBound.bounciness = 1;
+	// rightBound = new Sprite();
+	// rightBound.x = pegs[14].x + pegSpacing;
+	// rightBound.y = pegs[14].y - 2 * pegSpacing; 
+	// rightBound.collider = 'static';
+	// rightBound.rotation = -45;
+	// rightBound.height = 400;
+	// rightBound.color = 'gold';
+	// rightBound.bounciness = 1;
 }
 
 function draw() {
 	background('skyblue');
 
 	if (mouse.pressed()) {
+		pegs.color = 'gold';
 		createBall();
 	}
 }
@@ -68,15 +71,23 @@ function createBall() {
 	ball.y = 50;
 	ball.radius = 15;
 	ball.color = 'green';
-	ball.bounciness = .5;
+	ball.bounciness = 0.5;
+	ball.collided(pegs, updatePeg);
 	return ball;
 }
 
 function setupPegs() {
 	pegs = new Group();
 	pegs.radius = 5;
-	pegs.color = "yellow";
+	pegs.color = "gold";
 	pegs.collider = "static";
 	pegs.bounciness = 1;
 	return pegs;
+}
+
+function updatePeg(ball, peg) {
+	peg.color = 'red';
+	// peg.overlaps(ball);
+	// peg.visible = false;
+	// ball.velocity.mult(1.5);
 }
