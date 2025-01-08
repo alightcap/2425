46bdd2a8c22a97aae56d083df1335f7f1efe5db1
Andy Lightcap <andy.lightcap@gmail.com>
2025-01-08 15:47:28 -0500

    Finish Brick Breaker Notes

--- P5 Play/Brick Breaker/sketch.js
+++ P5 Play/Brick Breaker/sketch.js
@@ -1,7 +1,9 @@
 let backgroundImg;
 let ball;
+let bricks;
 let paddle;
 let walls;
+let score = 0;
 
 function preload() {
 	backgroundImg = loadImage('assets/Space Background.png');
@@ -11,16 +13,32 @@ function setup() {
 	new Canvas(800, 600);
 	displayMode('centered');
 
+	textAlign(CENTER);
+
 	paddle = createPaddle();
 	walls = createWalls();
 	ball = createBall();
+	bricks = createBricks();
+
+	ball.collides(bricks, hitBrick);
+
 	countdown();
 }
 
 function draw() {
 	background(backgroundImg);
 
+	fill("white");
+	textSize(20);
+	text("Score: " + score, 75, 50);
+
 	updatePaddle();
+	if (ball.y > height) {
+		endGame();
+	}
+	if (bricks.length < 1) {
+		endGame();
+	}
 }
 
 async function countdown() {
@@ -31,7 +49,7 @@ async function countdown() {
 function createBall() {
 	let b = new Sprite();
 
-	b.color = "lightgreen"
+	b.color = "turquoise";
 	b.x = width / 2;
 	b.y = height / 2;
 	b.diameter = 20;
@@ -39,6 +57,36 @@ function createBall() {
 	b.friction = 0;
 	b.direction = 80;
 	b.speed = 0;
+
+	return b;
+}
+
+function createBricks() {
+	let b = new Group();
+
+	b.color = "skyblue";
+	b.collider = "static";
+	b.width = 40;
+	b.height = 20;
+	// b.friction = 0;
+	// b.bounciness = 1;
+	b.tile = "-";
+
+	b.pointValue = 100;
+
+	let tiles = new Tiles(
+		[
+			"---------------",
+			"-----.....-----",
+			"---.........---",
+			"-.............-",
+			"---.........---",
+			"-----.....-----",
+			"---------------",
+		],
+		90, 100,
+		b.width + 4, b.height + 4
+	)
 
 	return b;
 }
@@ -67,6 +115,27 @@ function createWalls() {
 	new w.Sprite(width / 2, 0, width, 20);
 
 	return w;
+}
+
+function endGame() {
+	if (ball.y > height) {
+		textSize(50);
+		fill('red');
+		text("Game Over", width / 2, height / 2);
+	}
+
+	if (bricks.length < 1) {
+		textSize(50);
+		fill("green");
+		text("You Win!", width / 2, height / 2);
+	}
+
+	noLoop();
+}
+
+function hitBrick(ball, brick) {
+	score += brick.pointValue;
+	brick.remove();
 }
 
 function updatePaddle() {
