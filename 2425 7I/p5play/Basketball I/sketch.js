21674997eb6b45c026ed3702bcae3657b3a094a2
Andy Lightcap <andy.lightcap@gmail.com>
2025-01-16 12:17:45 -0500

    Basketball I through ground sensor

--- 2425 7I/p5play/Basketball I/sketch.js
+++ 2425 7I/p5play/Basketball I/sketch.js
@@ -1,6 +1,8 @@
 let player;
 let playerIdleAnimation;
 let playerRunAnimation;
+let gravityStrength;
+let ground;
 
 function preload() {
 	playerIdleAnimation = loadAni(
@@ -20,7 +22,11 @@ function setup() {
 	new Canvas(800, 600);
 	displayMode('centered');
 
+	gravityStrength = 10;
+	world.gravity.y = gravityStrength;
+
 	player = createPlayer();
+	ground = createGround();
 }
 
 function draw() {
@@ -29,14 +35,47 @@ function draw() {
 	playerController();
 }
 
+function createGround() {
+	let g = new Sprite();
+
+	g.x = width / 2;
+	g.y = height;
+	g.width = width;
+	g.height = 40;
+	g.collider = 'static';
+	g.color = 'green';
+	g.bounciness = 0;
+
+	return g;
+}
+
 function createPlayer() {
 	let p = new Sprite();
 
 	p.moveSpeed = 5;
+	p.xScale = 5;
+	p.jumpForce = 50;
 
-	p.scale = 5;
+	p.width = 9;
+	p.height = 9;
+	p.bounciness = 0;
+	p.bearing = -90;
+
+	p.scale = p.xScale;
 	p.addAni('run', playerRunAnimation);
 	p.addAni('idle', playerIdleAnimation);
+	// p.debug = true;
+
+	let g = new Sprite();
+	g.radius = 4;
+	g.collider = 'none';
+	g.x = p.x;
+	g.y = p.y + p.halfHeight;
+	g.visible = false;
+
+	p.groundSensor = g;
+	let j = new GlueJoint(p, p.groundSensor);
+	j.visible = false;
 
 	return p;
 }
@@ -52,12 +91,24 @@ function playerController() {
 		inputX = 1;
 	}
 
+	if (kb.presses('up')) {
+		player.applyForce(player.jumpForce);
+	}
+
 	player.vel.x = inputX * player.moveSpeed;
 
 	playerVisuals();
 }
 
 function playerVisuals() {
+	if (player.vel.x > 0) {
+		player.scale.x = player.xScale;
+	}
+
+	if (player.vel.x < 0) {
+		player.scale.x = -player.xScale;
+	}
+
 	if (player.isMoving) {
 		player.changeAni('run');
 	} else {
