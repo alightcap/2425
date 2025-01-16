6636ce676bf7577109e61b8b12d672529a95098c
Andy Lightcap <andy.lightcap@gmail.com>
2025-01-16 10:51:40 -0500

    Basketball H through ground sensor

--- 2425 7H/p5play/Basketball H/sketch.js
+++ 2425 7H/p5play/Basketball H/sketch.js
@@ -1,20 +1,33 @@
 let player;
 let playerIdleAnim;
+let playerRunAnim;
+let gravityStrength;
+let ground;
 
 function preload() {
 	playerIdleAnim = loadAni(
 		'assets/idle.png',
 		{ frameSize: [24, 24], frames: 3 }
-	)
+	);
 
 	playerIdleAnim.frameDelay = 6;
+
+	playerRunAnim = loadAni(
+		'assets/move.png',
+		{ frameSize: [24, 24], frames: 6 }
+	);
+
+	playerRunAnim.frameDelay = 6;
 }
 
 function setup() {
 	new Canvas(800, 600);
 	displayMode('centered');
+	gravityStrength = 10;
+	world.gravity.y = gravityStrength;
 
 	player = createPlayer();
+	ground = createGround();
 }
 
 function draw() {
@@ -23,13 +36,44 @@ function draw() {
 	playerController();
 }
 
+function createGround() {
+	let g = new Sprite();
+
+	g.x = width / 2;
+	g.y = height;
+	g.width = width;
+	g.height = 40;
+	g.collider = 'static';
+	g.color = 'green';
+	g.bounciness = 0;
+
+	return g;
+}
+
 function createPlayer() {
 	let p = new Sprite();
 
+	p.width = 9;
+	p.height = 9;
+
 	p.addAni('idle', playerIdleAnim);
-	p.scale = 5;
+	p.addAni('run', playerRunAnim);
+	p.xScale = 5;
+	p.scale = p.xScale;
+	p.bearing = -90;
+	p.bounciness = 0;
+	// p.debug = true;
 
 	p.moveSpeed = 5;
+	p.jumpForce = 50;
+
+	let groundSensor = new Sprite();
+	groundSensor.debug = true;
+	groundSensor.radius = 4;
+	groundSensor.collider = 'none';
+	groundSensor.x = p.x;
+	groundSensor.y = p.y + p.height;
+	p.groundSensor = groundSensor;
 
 	return p;
 }
@@ -45,5 +89,27 @@ function playerController() {
 		inputX = 1;
 	}
 
+	if (kb.presses('up')) {
+		player.applyForce(player.jumpForce);
+	}
+
 	player.vel.x = inputX * player.moveSpeed;
+
+	playerVisuals();
+}
+
+function playerVisuals() {
+	if (player.vel.x > 0) {
+		player.scale.x = player.xScale;
+	}
+
+	if (player.vel.x < 0) {
+		player.scale.x = -player.xScale;
+	}
+
+	if (player.isMoving) {
+		player.changeAni('run');
+	} else {
+		player.changeAni('idle');
+	}
 }
